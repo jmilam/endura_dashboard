2147160d1a648815c9d8f6113eecdca03698b81e
itadmin <itadmin@gso-linuxweb.endura.enduraproducts.com>
2016-11-30 11:22:31 -0500

    Add Calcluations for SRO OE Data

--- app/controllers/sros/order_entries_controller.rb
+++ app/controllers/sros/order_entries_controller.rb
@@ -1,6 +1,12 @@
 class Sros::OrderEntriesController < ApplicationController
 	def index
 	  #Initial Values set for variables
+	  @current_yr_summary = Hash.new
+	  @previous_yr_summary = Hash.new
+	  @current_year = Date.today.year
+	  @previous_year = Date.today.last_year.year
+	  @current_year_total = 0
+          @previous_year_total = 0
 	  @performance_data = Array.new
 	  @user_names = ['Order Type']
 	  @auto_orders = ['Auto Orders']
@@ -13,8 +19,36 @@ class Sros::OrderEntriesController < ApplicationController
 	  response = Net::HTTP.get(uri)
 	  json_response =  JSON.parse(response)
 	  @user_stats = json_response["userstats"]
+	  @sro_summary = json_response["sros"]
+
+          #This cycles the returned JSON data from QAD and builds an Array for Google Chart Visualization for the Summary Data
+	  @sro_summary.each do |summary|
+	    if @current_year == summary["sro-due-date"].to_date.year
+	      if @current_yr_summary.key?(summary["sro-taken"])
+                @current_yr_summary[summary["sro-taken"]].key?(summary["sro-type"]) ? @current_yr_summary[summary["sro-taken"]][summary["sro-type"]] += summary["sro-line-total"] : @current_yr_summary[summary["sro-taken"]] = {summary["sro-type"] => summary["sro-line-total"]}
+	        #@current_yr_summary[summary["sro-taken"]]["total"] += summary["sro-line-total"]
+	      else
+	        @current_yr_summary[summary["sro-taken"]] = summary["sro-taken"]
+	        @current_yr_summary[summary["sro-taken"]] = {summary["sro-type"] => summary["sro-line-total"]}
+		
+	      end
+	      #Add calculations to current Year totals
+	      @current_year_total += summary["sro-line-total"]
+	    else 
+	      if @previous_yr_summary.key?(summary["sro-taken"])
+                @previous_yr_summary[summary["sro-taken"]].key?(summary["sro-type"]) ? @previous_yr_summary[summary["sro-taken"]][summary["sro-type"]] += summary["sro-line-total"] : @previous_yr_summary[summary["sro-taken"]] = {summary["sro-type"] => summary["sro-line-total"]}
+                #@previous_yr_summary[summary["sro-taken"]]["total"] += summary["sro-line-total"]
+              else
+                @previous_yr_summary[summary["sro-taken"]] = summary["sro-taken"]
+                @previous_yr_summary[summary["sro-taken"]] = {summary["sro-type"] => summary["sro-line-total"]}
+              end
+
+	      #Add calculations to previous Year totals
+	      @previous_year_total += summary["sro-line-total"]
+            end
+	  end
 	  
-	  #This cycles the returned JSON data from QAD and builds an Array for Google Chart Visualization
+	  #This cycles the returned JSON data from QAD and builds an Array for Google Chart Visualization for the Performance Snapshot
 	  @user_stats.each do |stats|
 	    @user_names << stats["t_userid"]
 	    @auto_orders << (stats["t_edi_ord"] + stats["t_scn_ord"])
