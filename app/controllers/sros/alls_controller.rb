17a0089027188d99101e6e6a1105a9791c268586
itadmin <itadmin@gso-linuxweb.endura.enduraproducts.com>
2016-12-12 17:15:02 -0500

    Add Data Parsing to SRO All

--- app/controllers/sros/alls_controller.rb
+++ app/controllers/sros/alls_controller.rb
@@ -1,20 +1,56 @@
 class Sros::AllsController < ApplicationController
   def index
-    @sro_summary = Hash.new
-    @customers = Array.new
+    @sro_by_division = Hash.new
+    @sro_by_type = Hash.new
+    @sro_by_prod_line = Hash.new
+    @sro_by_dept = Hash.new
     @start_date = (Date.today.beginning_of_week - 1.week).strftime("%D")
     @end_date = (Date.today.end_of_week - 1.week).strftime("%D")
     
     uri = URI(self.api_url + "xxapioesrodashboard.p?start=#{@start_date}&end=#{@end_date}")
     response = Net::HTTP.get(uri)
     JSON.parse(response)["sros"].each do |sros|
-      if @sro_summary.keys.include?(sros["sro-name"])
-	@sro_summary[sros["sro-name"]][sros["srod-site"]].nil? ? @sro_summary[sros["sro-name"]][sros["srod-site"]] = sros["sro-line-total"] : @sro_summary[sros["sro-name"]][sros["srod-site"]] = Sro.add(@sro_summary[sros["sro-name"]][sros["srod-site"]], sros["sro-line-total"])
-        #@sro_summary[sros["sro-name"]]["grand_total"] += sros["srod-line-total"]
-	#@sro_summary[sros["sro-name"]]["grand_total"].nil? ? @sro_summary[sros["sro-name"]]["grand_total"] = sros["sro-line-total"] : Sro.add(@sro_summary[sros["sro-name"]]["grand_total"], sros["sro-line-total"])
+      if @sro_by_division.keys.include?(sros["sro-div"])
+        @sro_by_division[sros["sro-div"]] = Sro.calculate_all_sros(@sro_by_division[sros["sro-div"]], sros)
       else
-	@sro_summary[sros["sro-name"]] = {sros["srod-site"] => sros["sro-line-total"]}
-	@sro_summary[sros["sro-name"]]["grand_total"].nil? ? @sro_summary[sros["sro-name"]]["grand_total"] = sros["sro-line-total"] : Sro.add(@sro_summary[sros["sro-name"]]["grand_total"], sros["sro-line-total"])
+        unless sros["sro-div"].empty?
+          @sro_by_division[sros["sro-div"]] = {curr_month: 0.00, prev_month: 0.00, curr_ytd: 0.00, prev_ytd: 0.00, prev_year: 0.00}
+          @sro_by_division[sros["sro-div"]] = Sro.calculate_all_sros(@sro_by_division[sros["sro-div"]], sros)      
+        end
+      end
+
+      if @sro_by_type.keys.include?(sros["sro-div"])
+        if @sro_by_type[sros["sro-div"]][sros["sro-type"]].nil?
+          @sro_by_type[sros["sro-div"]] = {sros["sro-type"] => {curr_month: 0.00, prev_month: 0.00, curr_ytd: 0.00, prev_ytd: 0.00, prev_year: 0.00}}
+	else
+	  @sro_by_type[sros["sro-div"]][sros["sro-type"]] = Sro.calculate_all_sros(@sro_by_type[sros["sro-div"]][sros["sro-type"]], sros)
+	end
+      else
+    	#@sro_by_type["sro-div"] = Sro.create_new_sro_data(sros["sro-div"], sros["sro-type"], @sro_by_type)
+        @sro_by_type[sros["sro-div"]] = {sros["sro-type"] => {curr_month: 0.00, prev_month: 0.00, curr_ytd: 0.00, prev_ytd: 0.00, prev_year: 0.00}}
+        @sro_by_type[sros["sro-div"]][sros["sro-type"]] = Sro.calculate_all_sros(@sro_by_type[sros["sro-div"]][sros["sro-type"]],sros)
+      end
+
+      if @sro_by_prod_line.keys.include?(sros["sro-div"])
+	if @sro_by_prod_line[sros["sro-div"]][sros["sro-prodline"]].nil?
+          @sro_by_prod_line[sros["sro-div"]] = {sros["sro-prodline"] => {curr_month: 0.00, prev_month: 0.00, curr_ytd: 0.00, prev_ytd: 0.00, prev_year: 0.00}}
+	else
+          @sro_by_prod_line[sros["sro-div"]][sros["sro-prodline"]] = Sro.calculate_all_sros(@sro_by_prod_line[sros["sro-div"]][sros["sro-prodline"]], sros)
+	end
+      else
+        @sro_by_prod_line[sros["sro-div"]] = {sros["sro-prodline"] => {curr_month: 0.00, prev_month: 0.00, curr_ytd: 0.00, prev_ytd: 0.00, prev_year: 0.00}}
+        @sro_by_prod_line[sros["sro-div"]][sros["sro-prodline"]] = Sro.calculate_all_sros(@sro_by_prod_line[sros["sro-div"]][sros["sro-prodline"]],sros)
+      end
+
+      if @sro_by_dept.keys.include?(sros["sro-div"])
+ 	if @sro_by_dept[sros["sro-div"]][sros["xxsro-so-dept"]].nil?
+          @sro_by_dept[sros["sro-div"]] = {sros["xxsro-so-dept"] => {curr_month: 0.00, prev_month: 0.00, curr_ytd: 0.00, prev_ytd: 0.00, prev_year: 0.00}}
+        else
+	  @sro_by_dept[sros["sro-div"]][sros["xxsro-so-dept"]] = Sro.calculate_all_sros(@sro_by_dept[sros["sro-div"]][sros["xxsro-so-dept"]], sros)
+	end
+      else
+        @sro_by_dept[sros["sro-div"]] = {sros["xxsro-so-dept"] => {curr_month: 0.00, prev_month: 0.00, curr_ytd: 0.00, prev_ytd: 0.00, prev_year: 0.00}}
+	@sro_by_dept[sros["sro-div"]][sros["xxsro-so-dept"]] = Sro.calculate_all_sros(@sro_by_dept[sros["sro-div"]][sros["xxsro-so-dept"]],sros)
       end
     end
   end
--- app/models/sro.rb
+++ app/models/sro.rb
@@ -2,4 +2,32 @@ class Sro
   def self.add(total, number)
     (total += number).round(2)
   end
+  def self.calculate_all_sros(sro_hash, sro_value)
+    if Date.today.year.to_s == sro_value["sro-due-date"].to_date.year.to_s
+      #Add it to Year To Date
+      sro_hash[:curr_ytd] += sro_value["sro-line-total"].round(2)
+      #Add it to Previous Month
+      sro_hash[:prev_month] += sro_value["sro-line-total"].round(2) if self.prev_month(sro_value["sro-due-date"])
+    elsif (Date.today.year - 1).to_s == sro_value["sro-due-date"].to_date.year.to_s
+      #Add it to Previous Year
+      p sro_hash
+      sro_hash[:prev_year] += sro_value["sro-line-total"].round(2)
+      #Add it to Previous YTD
+      sro_hash[:prev_ytd] += sro_value["sro-line-total"].round(2) if self.year_to_date(sro_value["sro-due-date"])
+    else
+    end
+    sro_hash    
+  end
+  def self.year_to_date(due_date)
+    due_date.to_date.month <= Date.today.month
+  end
+  def self.prev_month(due_date)
+    due_date.to_date.month == Date.today.month - 1
+  end
+  def self.add_to_current_sro_data
+    
+  end
+  def self.create_new_sro_data(key, value, data_var)
+    data_var[key] = {value => {curr_month: 0.00, prev_month: 0.00, curr_ytd: 0.00, prev_ytd: 0.00, prev_year: 0.00}}    
+  end
 end
