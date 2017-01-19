5015c44c27dcd45255afbb00f6ec458bbe8c3ab6
Jason Milam <jasonlmilam@gmail.com>
2017-01-19 08:26:27 -0500

    Fix spreadsheet workbook name

--- app/controllers/sros/report_card_controller.rb
+++ app/controllers/sros/report_card_controller.rb
@@ -2,12 +2,11 @@ class Sros::ReportCardController < ApplicationController
 	skip_before_filter :verify_authenticity_token  
 	require 'csv'
 	def create
-		params[:emp_report_card]
 		report_date = Date.parse(params["t_month"]).strftime("%b '%y") unless params["t_month"].nil?
 		spreadsheet = StringIO.new
 		Spreadsheet.client_encoding = 'UTF-8'
 		book = Spreadsheet::Workbook.new
-		sheet1 = book.create_worksheet :name => params[:emp_name].to_s
+		sheet1 = book.create_worksheet :name => "Report Card #{Date.today.strftime("%m-%d-%Y")}"
 		sheet1.row(0).replace ["Employee Name", "Total Orders", "Manual orders", "Auto Orders", "Total Orders %", "Total Lines", "Manual Lines", "Auto Lines", "Total Line %", "Customer Count", "Total Sro's", "Report Month"]
 		
 		row_count = 1
