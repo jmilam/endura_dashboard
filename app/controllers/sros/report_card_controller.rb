e3ceb220d7525abec0aded74d450e408763a91b7
Jason Milam <jasonlmilam@gmail.com>
2017-06-01 15:39:41 -0400

    Fix OE.xls export to add dynamic email and also format the xls file

--- app/controllers/sros/report_card_controller.rb
+++ app/controllers/sros/report_card_controller.rb
@@ -3,25 +3,30 @@ class Sros::ReportCardController < ApplicationController
 	require 'csv'
 	require 'net/http/post/multipart'
 	def create
+		email = nil
 		response = nil
 		report_date = Date.parse(params["t_month"]).strftime("%b '%y") unless params["t_month"].nil?
 		spreadsheet = StringIO.new
 		Spreadsheet.client_encoding = 'UTF-8'
 		book = Spreadsheet::Workbook.new
 		sheet1 = book.create_worksheet :name => "Report Card #{Date.today.strftime("%m-%d-%Y")}"
-		sheet1.row(0).replace ["Employee Name", "Total Orders", "Manual orders", "Auto Orders", "Total Orders %", "Total Lines", "Manual Lines", "Auto Lines", "Total Line %", "Customer Count", "Total Sro's", "Report Month"]
+		sheet1.row(0).replace ["Employee Name", "Total Orders", "Manual orders", "Manual Lag Time (Days)", "Scaned Orders", "Scanned Lag Time (Days)", "Total Orders Complete %", "Total Lines", "Manual Lines", "Auto Lines", "Total Line Complete %", "Customer Count", "Total Order $", "Total Sro's", "Report Month"]
 		
 		row_count = 1
 
 		params[:emp_report_card].each do |key, value|
-			sheet1.row(row_count).replace [value["emp"], value["total_order_count"], value["man_orders"], value["auto_orders"], value["order_perc"], value["total_lines"],
-																		 value["man_lines"], value["auto_lines"], value["line_perc"], value["cust_count"], value["sro_total"], value["stat_month"]]
+			if email.nil?
+				email = value["email"]
+			end
+			sheet1.row(row_count).replace [value["emp"], value["total_order_count"], value["man_orders"], value["man_lag_time"], value["auto_orders"], value["scan_lag_time"], value["order_perc"], value["total_lines"],
+																		 value["man_lines"], value["auto_lines"], value["line_perc"], value["cust_count"], value["sro_dol_total"], value["sro_total"], value["stat_month"]]
 			row_count += 1
 		end
+
 		file_loc = "public/report cards/oe/OE-Emp-Report-Card-#{Date.today.strftime("%m-%d-%Y")}.xls"
 		book.write file_loc
 
-	  req_params = {from: "oe_sro_dashboard@enduraproducts.com", to: "djorgenson@enduraproducts.com", subject: "OE Report Card", chart_data: {data: Formatter.from_oe_report_card(params[:emp_report_card])}}
+	  req_params = {from: "oe_sro_dashboard@enduraproducts.com", to: "#{email}", subject: "OE Report Card", chart_data: {data: Formatter.from_oe_report_card(params[:emp_report_card])}}
 		uri = URI(self.api_url + "/email/order_entry/report_card")
 
 	  File.open(file_loc) do |xls_file|
