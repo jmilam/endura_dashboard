4685091c4b8e09bb6edf220faade4324fc1788f7
Jason Milam <jasonlmilam@gmail.com>
2018-01-03 09:38:45 -0500

    Add variables to report mailer

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -3,6 +3,14 @@ class ReportMailer < ApplicationMailer
 	default from: 'alerts@enduraproducts.com'
  
   def backlog_overview_email
+  	@counter = 0
+		@header = true
+		@sub_header = false
+
+		@body_next = false
+		@header_next = true
+		@new_table = false
+
   	mail(to: 'jasonlmilam@gmail.com', subject: 'Backlog Overview Email')
   end
 end
