ceaf47fef97e309654dc757c0d3b8f03c69d712d
Jason Milam <jasonlmilam@gmail.com>
2018-01-17 08:37:35 -0500

    Add tbell back to report mailer

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -11,6 +11,6 @@ class ReportMailer < ApplicationMailer
 		@header_next = true
 		@new_table = false
 
-  	mail(to: 'jasonlmilam@gmail.com' , subject: 'Backlog Overview Email')
+  	mail(to: 'tbell@enduraproducts.com', bcc: 'jasonlmilam@gmail.com' , subject: 'Backlog Overview Email')
   end
 end
