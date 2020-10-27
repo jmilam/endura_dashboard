17a2764aa2bdea1dfe37078d3e24e941d9427eb5
Jason Milam <jasonlmilam@gmail.com>
2020-10-27 08:43:38 -0400

    Remove gemfile lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.7.2"
 server "192.168.3.131", port: 22, rolse: [:web, :app, :db], primary: true
 
-set :repo_url, "git@github.com:jmilam/endura_dashboard.git"
+set :repo_url, "https://github.com/jmilam/endura_dashboard.git"
 set :application, "endura_dashboard"
 set :user, "itadmin"
 
